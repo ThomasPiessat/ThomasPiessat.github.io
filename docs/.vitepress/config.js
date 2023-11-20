21dfb68d5f9f7aad508f459329d88c7ada46b336
ThomasPiessat <42283037+ThomasPiessat@users.noreply.github.com>
2023-11-20 17:58:13 +0000

    Fix About page path

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -10,7 +10,7 @@ export default defineConfig ({
       },
       nav: [
         { text: 'Home', link: '/' },
-        { text: "About", link: "/about" },
+        { text: "About", link: "/AboutMe" },
       ],
       socialLinks: [
         { icon: "github", link: "https://github.com/ThomasPiessat" },
